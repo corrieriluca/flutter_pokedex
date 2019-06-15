f67803c5058b34a14e6dcdd1d7f73c1b6d51be14
Luca Corrieri <luca.corrieri@epita.fr>
2019-06-15 20:32:16 +0200

    Tabs (colours need to be changed) and AppBar added

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(PokedexApp());
@@ -7,14 +8,78 @@ class PokedexApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Pokédex',
-      home: Scaffold(
-        appBar: AppBar(
-          title: new Text('Pokédex'),
-        ),
-        body: Center(
-          child: Text('Hello, World !'),
-        ),
+      theme: ThemeData(
+        primaryColor: Colors.red.shade800,
       ),
+      home: DefaultTabController(
+        length: 2,
+        child: MainView(),
+      ),
+    );
+  }
+}
+
+class MainView extends StatelessWidget {
+  void _searchPokemon() {}
+
+  void _settings() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Pokédex'),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.search),
+              onPressed: _searchPokemon,
+            ),
+            IconButton(
+              icon: Icon(Icons.settings),
+              onPressed: _settings,
+            ),
+          ],
+          bottom: TabBar(
+            isScrollable: false,
+            tabs: <Tab>[
+              Tab(text: 'POKÉMON'),
+              Tab(text: 'FAVOURITES'),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: <Widget>[
+            PokedexView(),
+            FavouritesView(),
+          ],
+        ));
+  }
+}
+
+class PokedexView extends StatefulWidget {
+  @override
+  createState() => PokedexViewState();
+}
+
+class PokedexViewState extends State<PokedexView> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Pokédex View here...'),
+    );
+  }
+}
+
+class FavouritesView extends StatefulWidget {
+  @override
+  createState() => FavouritesViewState();
+}
+
+class FavouritesViewState extends State<FavouritesView> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Favourites Viex here...'),
     );
   }
 }
